c1ebcdb6c34926273efe0b7fe6b33c94987579e4
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2022-02-01 20:44:42 +0100

    procfile + port on app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app=express();
 const server = http.createServer(app)
